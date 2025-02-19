d1e12036b5a9ee1c724306b06641b1af6a3946db
Freya Arbjerg <freya@arbjerg.dev>
2025-02-19 18:11:56 +0100

    Batch request polylines

--- src/polyline.rs
+++ src/polyline.rs
@@ -1,23 +1,29 @@
 use reqwest::Client;
 use serde_json::Value;
 use crate::status::Status;
+use itertools::Itertools;
 
 /// The path takes a comma-separated list of status IDs
 const POLYLINE_URL: &str = "https://traewelling.de/api/v1/polyline/";
+/// https://github.com/Traewelling/traewelling/blob/develop/app/Http/Controllers/API/v1/StatusController.php#L500
+const POLYLINE_PAGE_COUNT: usize = 50;
 
 pub async fn get_polylines(client: &Client, statuses: &Vec<Status>) -> Vec<Value> {
     let mut lines: Vec<Value> = Vec::new();
-    for (i, status) in statuses.iter().enumerate() {
-        let data = get_polyline(client, &status).await["data"].take();
+    let pages = (statuses.len() as f32 / POLYLINE_PAGE_COUNT as f32).ceil() as usize;
+
+    for (page, chunk) in statuses.chunks(POLYLINE_PAGE_COUNT).enumerate() {
+        let ids = chunk.iter().map(|s| s.id).join(",");
+        let data = get_polyline_page(client, format!("{}{}", POLYLINE_URL, ids)).await["data"].take();
         lines.push(data);
-        println!("Fetched polyline {:04} out of {}", i + 1, statuses.len());
+        println!("Fetched polyline page {} out of {}", page + 1, pages);
     }
     lines
 }
 
-async fn get_polyline(client: &Client, status: &Status) -> Value {
+async fn get_polyline_page(client: &Client, url: String) -> Value {
     loop {
-        let response = crate::http::handle_request(client.get(format!("{}{}", POLYLINE_URL, status.id))).await;
+        let response = crate::http::handle_request(client.get(url)).await;
 
         let json = response
             .text()
